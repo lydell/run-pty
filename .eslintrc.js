34231827a22aab2b8d05061317d009e09bace964
Simon Lydell <simon.lydell@gmail.com>
2020-07-05 20:47:44 +0200

    It works!

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended"],
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    "arrow-body-style": "error",
+    curly: "error",
+    "dot-notation": "error",
+    "no-shadow": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "object-shorthand": "error",
+    "one-var": ["error", "never"],
+    "prefer-arrow-callback": "error",
+    "prefer-destructuring": ["error", { array: false, object: true }],
+    "prefer-rest-params": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    eqeqeq: "error",
+    strict: "error",
+  },
+};
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+"use strict";
+
 const pty = require("node-pty");
 const readline = require("readline");
+
+const labels = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const help = `
 Run several commands concurrently.
@@ -15,32 +19,72 @@ run-pty @ ./compile.bash --root / @ ./report_progress.bash --unit % @ ping local
 Note: A command is a file followed by arguments – not shell script code.
 `.trim();
 
-function run() {
-  const parseResult = parseArgs(process.argv.slice(2));
+const runningIndicator = "🟢";
 
-  switch (parseResult.tag) {
-    case "Help":
-      console.log(help);
-      process.exit(0);
-      break;
+const exitIndicator = (exitCode) => (exitCode === 0 ? "⚪" : "🔴");
 
-    case "Parsed":
-      runCommands({
-        dashboardKey: parseResult.dashboardKey,
-        commands: parseResult.commands,
-      });
-      break;
+function firstHistoryLine(name) {
+  return `${runningIndicator} ${name}\n`;
+}
 
-    case "Error":
-      console.error(parseResult.message);
-      process.exit(1);
-      break;
+// Newlines at start/end are wanted here.
+function exitText(commandName, exitCode) {
+  return `
+${exitIndicator(exitCode)} ${commandName}
+exit ${exitCode}
+
+Press Enter to restart.
+Press ctrl+z to go to the dashboard.
+Press ctrl+c to exit all.
+`;
+}
+
+function statusText(status) {
+  switch (status.tag) {
+    case "Running":
+      return `${runningIndicator} pid ${status.terminal.pid}`;
+
+    case "Exit":
+      return `${exitIndicator(status.exitCode)} exit ${status.exitCode}`;
 
     default:
-      console.error(`Unknown parseResult`, parseResult);
-      process.exit(1);
-      break;
+      throw new Error("Unknown command status", status);
   }
+}
+
+function drawDashboard(commands) {
+  const lines = commands.map((command) =>
+    truncate(
+      [command.label, statusText(command.status), command.name].join("  "),
+      process.stdout.columns
+    )
+  );
+
+  return `
+${lines.join("\n")}
+
+Press the digit/letter of a command to switch to it.
+Press ctrl+c to exit all.
+`.trim();
+}
+
+function truncate(string, maxLength) {
+  return string.length <= maxLength
+    ? string
+    : `${string.slice(0, maxLength - 1)}…`;
+}
+
+function commandToPresentationName(command) {
+  return command
+    .map((part) =>
+      /^[\w./-]+$/.test(part) ? part : `'${part.replace(/'/g, "’")}'`
+    )
+    .join(" ");
+}
+
+function keypressToString(keypress) {
+  const name = keypress.name || keypress.sequence || "unknown";
+  return keypress.ctrl ? `ctrl+${name}` : name;
 }
 
 function parseArgs(args) {
@@ -54,7 +98,7 @@ function parseArgs(args) {
     return {
       tag: "Error",
       message:
-        "The first argument is the delimiter to use between commands.\nIt must not be empty or 0-9/a-z/underscores/dashes only.\nMaybe try % as delimiter?",
+        "The first argument is the delimiter to use between commands.\nIt must not be empty or a-z/0-9/underscores/dashes only.\nMaybe try % as delimiter?",
     };
   }
 
@@ -89,63 +133,257 @@ function parseArgs(args) {
   };
 }
 
-function setupStdin() {
-  process.stdin.setRawMode(true);
-  process.stdin.setEncoding("utf8");
-  readline.emitKeypressEvents(process.stdin);
-}
+class Command {
+  constructor({ label, file, args, onData, onExit }) {
+    this.label = label;
+    this.file = file;
+    this.args = args;
+    this.name = commandToPresentationName([file, ...args]);
+    this.onData = onData;
+    this.onExit = onExit;
+    this.history = [];
+    this.status = { tag: "Exit", exitCode: 0 };
+    this.start();
+  }
 
-function commandToPresentationName(command) {
-  return command
-    .map((part) =>
-      /^[\w-]+$/.test(part) ? part : `'${part.replace(/'/g, "’")}'`
-    )
-    .join(" ");
-}
+  start() {
+    if (this.status.tag === "Running") {
+      throw new Error(
+        `pty already running with pid ${this.status.terminal.pid} for: ${this.name}`
+      );
+    }
 
-function runCommands({ dashboardKey, commands }) {
-  setupStdin();
+    this.history = [firstHistoryLine(this.name)];
 
-  const terminals = commands.map((command) => ({
-    name: commandToPresentationName(command),
-    history: [],
-    terminal: pty.spawn(command[0], command.slice(1), {
+    const terminal = pty.spawn(this.file, this.args, {
       cols: process.stdout.columns,
       rows: process.stdout.rows,
-    }),
-  }));
+    });
 
-  let current =
-    commands.length === 1 ? { tag: "Command", index: 0 } : { tag: "Dashboard" };
+    const disposeOnData = terminal.onData((data) => {
+      this.history.push(data);
+      this.onData(data);
+    });
+
+    const disposeOnExit = terminal.onExit(({ exitCode }) => {
+      disposeOnData.dispose();
+      disposeOnExit.dispose();
+      this.status = { tag: "Exit", exitCode };
+      this.onExit(exitCode);
+    });
+
+    this.status = { tag: "Running", terminal };
+  }
+
+  log(data) {
+    this.history.push(data);
+    this.onData(data);
+  }
+}
+
+function runCommands(rawCommands) {
+  let current = { tag: "Dashboard" };
+
+  const switchToDashboard = () => {
+    current = { tag: "Dashboard" };
+    console.clear();
+    console.log(drawDashboard(commands));
+  };
+
+  const switchToCommand = (index) => {
+    const command = commands[index];
+    current = { tag: "Command", index };
+    console.clear();
+    for (const data of command.history) {
+      process.stdout.write(data);
+    }
+  };
+
+  const killAll = () => {
+    for (const command of commands) {
+      if (command.status.tag === "Running") {
+        command.status.terminal.kill();
+      }
+    }
+    process.exit(0);
+  };
+
+  const commands = rawCommands.map(
+    ([file, ...args], index) =>
+      new Command({
+        label: labels[index] || "",
+        file,
+        args,
+        onData: (data) => {
+          if (current.tag === "Command" && current.index === index) {
+            process.stdout.write(data);
+          }
+        },
+        onExit: (exitCode) => {
+          switch (current.tag) {
+            case "Command": {
+              const command = commands[current.index];
+              command.log(exitText(command.name, exitCode));
+              break;
+            }
+
+            case "Dashboard":
+              // Redraw dashboard.
+              switchToDashboard();
+              break;
+
+            default:
+              throw new Error("Unknown current", current);
+          }
+        },
+      })
+  );
 
   process.stdout.on("resize", () => {
-    terminal.resize(process.stdout.columns, process.stdout.rows);
-  });
-
-  process.stdin.setRawMode(true);
-  process.stdin.setEncoding("utf8");
-  process.stdin.on("data", (data) => {
-    if (data === "\x03") {
-      terminal.kill();
-    } else if (data === "\t") {
-      console.clear();
-      for (const data of history) {
-        process.stdout.write(data);
+    for (const command of commands) {
+      if (command.status.tag === "Running") {
+        command.status.terminal.resize(
+          process.stdout.columns,
+          process.stdout.rows
+        );
       }
-    } else {
-      terminal.write(data);
+    }
+
+    if (current.tag === "Dashboard") {
+      // Redraw dashboard.
+      switchToDashboard();
     }
   });
 
-  const dispose1 = terminal.onData((data) => {
-    history.push(data);
-    process.stdout.write(data);
+  process.stdin.setRawMode(true);
+  process.stdin.setEncoding("utf8");
+  readline.emitKeypressEvents(process.stdin);
+
+  process.stdin.on("data", (data) => {
+    if (
+      current.tag === "Command" &&
+      commands[current.index].status.tag === "Running"
+    ) {
+      commands[current.index].status.terminal.write(data);
+    }
   });
 
-  const dispose2 = terminal.onExit(({ exitCode, signal }) => {
-    console.log("Exit", exitCode, signal);
-    process.exit(exitCode);
+  process.stdin.on("keypress", (_, keypress) => {
+    onKeypress(
+      keypress,
+      current,
+      commands,
+      switchToDashboard,
+      switchToCommand,
+      killAll
+    );
   });
+
+  if (commands.length === 1) {
+    switchToCommand(0);
+  } else {
+    switchToDashboard();
+  }
+}
+
+function onKeypress(
+  keypress,
+  current,
+  commands,
+  switchToDashboard,
+  switchToCommand,
+  killAll
+) {
+  const keypressString = keypressToString(keypress);
+
+  switch (current.tag) {
+    case "Command": {
+      const command = commands[current.index];
+      switch (command.status.tag) {
+        case "Running":
+          switch (keypressString) {
+            case "ctrl+c":
+              command.status.terminal.kill();
+              break;
+
+            case "ctrl+z":
+              switchToDashboard();
+              break;
+          }
+          break;
+
+        case "Exit":
+          switch (keypressString) {
+            case "ctrl+c":
+              killAll();
+              break;
+
+            case "ctrl+z":
+              switchToDashboard();
+              break;
+
+            case "return":
+              command.start();
+              command.history.unshift("\n");
+              for (const data of command.history) {
+                process.stdout.write(data);
+              }
+              break;
+          }
+          break;
+
+        default:
+          throw new Error("Unknown command status", command);
+      }
+      break;
+    }
+
+    case "Dashboard":
+      switch (keypressString) {
+        case "ctrl+c":
+          killAll();
+          break;
+
+        default: {
+          const commandIndex = commands.findIndex(
+            (command) => command.label === keypressString
+          );
+          if (commandIndex !== -1) {
+            switchToCommand(commandIndex);
+          }
+          break;
+        }
+      }
+      break;
+
+    default:
+      throw new Error("Unknown current", current);
+  }
+}
+
+function run() {
+  const parseResult = parseArgs(process.argv.slice(2));
+
+  switch (parseResult.tag) {
+    case "Help":
+      console.log(help);
+      process.exit(0);
+      break;
+
+    case "Parsed":
+      runCommands(parseResult.commands);
+      break;
+
+    case "Error":
+      console.error(parseResult.message);
+      process.exit(1);
+      break;
+
+    default:
+      console.error("Unknown parseResult", parseResult);
+      process.exit(1);
+      break;
+  }
 }
 
 run();
